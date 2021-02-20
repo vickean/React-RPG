3aae74783f56ab923a1fa97e3290c0eea97e0449
Victor Kean <vickean@gmail.com>
2021-02-21 04:24:19 +0800

    Add keyboard input detection

--- src/components/World/World.tsx
+++ src/components/World/World.tsx
@@ -1,16 +1,12 @@
-import React, { Fragment, useCallback, useEffect, useReducer } from "react";
+import React, { Fragment, useCallback, useEffect, useReducer, useRef } from "react";
 import "./World.css";
-
-interface CharacterProps {
-   facing: string;
-   walking: boolean;
-}
-
 interface WorldState {
    posX: number;
    posY: number;
    heldDirections: string[];
    speed: number;
+   facing: string;
+   walking: string;
 }
 
 interface Action {
@@ -35,9 +31,14 @@ const keys: { [index: string]: string } = {
 enum ActionType {
    UpdateCharacterPosition = "UPDATE_CHARACTER_POSITION",
    UpdateHeldDirections = "UPDATE_HELD_DIRECTIONS",
+   UpdateFacingWalking = "UPDATE_FACING_WALKING",
 }
 
 export default function World() {
+   const character = document.querySelector(".character");
+   const map = document.querySelector(".map");
+   const camera = useRef<HTMLDivElement>(null);
+
    const reducer = (state: WorldState, action: Action): WorldState => {
       switch (action.type) {
          case ActionType.UpdateCharacterPosition: {
@@ -57,6 +58,20 @@ export default function World() {
          case ActionType.UpdateHeldDirections: {
             return { ...state, heldDirections: action.payload };
          }
+         case ActionType.UpdateFacingWalking: {
+            let newFacing: string = state.facing;
+            let newWalking: string = state.walking;
+
+            if ("facing" in action.payload) {
+               newFacing = action.payload.facing;
+            }
+
+            if ("walking" in action.payload) {
+               newWalking = action.payload.walking;
+            }
+
+            return { ...state, facing: newFacing, walking: newWalking };
+         }
          default:
             return state;
       }
@@ -67,11 +82,13 @@ export default function World() {
       posY: 0,
       heldDirections: [],
       speed: 1,
+      facing: "down",
+      walking: "false",
    };
 
    const [state, dispatch] = useReducer(reducer, initialState);
 
-   const onKeyDown = useCallback((event: KeyboardEvent) => {
+   const onKeyDown = (event: React.KeyboardEvent) => {
       const heldDirections = [...state.heldDirections];
       const dir = keys[event.key];
 
@@ -81,13 +98,21 @@ export default function World() {
             type: ActionType.UpdateHeldDirections,
             payload: heldDirections,
          });
+         dispatch({
+            type: ActionType.UpdateFacingWalking,
+            payload: {
+               facing: dir,
+            },
+         });
       }
-   }, []);
+   };
 
-   const onKeyUp = useCallback((event: KeyboardEvent) => {
+   const onKeyUp = (event: React.KeyboardEvent) => {
       const heldDirections = [...state.heldDirections];
       const dir = keys[event.key];
       const index = heldDirections.indexOf(dir);
+
+      console.log("KEYUP>>> ", heldDirections, state.heldDirections);
 
       if (index > -1) {
          heldDirections.splice(index, 1);
@@ -96,28 +121,90 @@ export default function World() {
             payload: heldDirections,
          });
       }
-   }, []);
+   };
 
    const placeCharacter = () => {
       const pixelSize = parseInt(
          getComputedStyle(document.documentElement).getPropertyValue("--pixel-size")
       );
-      console.log("PIXELSIZE>>> ", pixelSize);
+
+      const held_direction = state.heldDirections[0];
+      if (held_direction) {
+         if (held_direction === directions.RIGHT) {
+            dispatch({
+               type: ActionType.UpdateCharacterPosition,
+               payload: {
+                  posX: state.posX + state.speed,
+               },
+            });
+         }
+         if (held_direction === directions.LEFT) {
+            dispatch({
+               type: ActionType.UpdateCharacterPosition,
+               payload: {
+                  posX: state.posX - state.speed,
+               },
+            });
+         }
+         if (held_direction === directions.DOWN) {
+            dispatch({
+               type: ActionType.UpdateCharacterPosition,
+               payload: {
+                  posX: state.posY + state.speed,
+               },
+            });
+         }
+         if (held_direction === directions.UP) {
+            dispatch({
+               type: ActionType.UpdateCharacterPosition,
+               payload: {
+                  posX: state.posX - state.speed,
+               },
+            });
+         }
+      }
+
+      console.log(held_direction, state.heldDirections);
+
+      dispatch({
+         type: ActionType.UpdateFacingWalking,
+         payload: {
+            walking: held_direction ? "true" : "false",
+         },
+      });
+   };
+
+   const focusOnCamera = () => {
+      if (null !== camera.current) {
+         camera.current.focus();
+      }
    };
 
    useEffect(() => {
-      document.addEventListener("keydown", onKeyDown);
-      document.addEventListener("keyup", onKeyUp);
-   }, []);
+      focusOnCamera();
+   });
 
    useEffect(() => {
       placeCharacter();
-   });
+   }, [state.heldDirections]);
+
+   useEffect(() => {
+      character?.setAttribute("data-facing", state.facing);
+      character?.setAttribute("data-walking", state.walking);
+   }, [state.facing, state.walking]);
 
    return (
       <Fragment>
-         <div className="camera">
-            <Map />
+         <div className="container" onClick={focusOnCamera}>
+            <div
+               className="camera"
+               onKeyDown={onKeyDown}
+               onKeyUp={onKeyUp}
+               tabIndex={-1}
+               ref={camera}
+            >
+               <Map />
+            </div>
          </div>
       </Fragment>
    );
@@ -126,14 +213,12 @@ export default function World() {
 const Map = () => {
    return (
       <div className="map pixel-art">
-         <Character facing="down" walking={true} />
+         <Character />
       </div>
    );
 };
 
-const Character = (props: CharacterProps) => {
-   const { facing, walking } = props;
-
+const Character = () => {
    return (
       <div className="character">
          <CharacterShadow />
